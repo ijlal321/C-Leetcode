d3fc03ab487e80410de2ec284e09ef474846c9a0
ijlal123 <ijlal.tanveer291@gmail.com>
2025-07-16 15:34:07 +0500

    completed static lib challenge

---
+++ lib challenge/calculator.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+
+int add(int num );
+int sub(int num );
+int mul(int num );
+
+
+#endif
---
+++ lib challenge/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "calculator.h"
+
+int main(){
+    printf("%d\n", add(2));
+    printf("%d\n", mul(2));
+    printf("%d\n", sub(2));
+}
